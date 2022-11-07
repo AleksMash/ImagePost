ae7626a900d69d90d5e0ac967261f5acfaf2d6f3
AleksMash <amashukov@yandex.ru>
2022-11-08 02:48:51 +0600

    publish photo to Telegram channel

--- telbot.py
+++ telbot.py
@@ -3,3 +3,4 @@ import telegram
 
 bot=telegram.Bot(token='2081633148:AAHT53UQUP_e0raz82jOzovx6-3e8OM7Iik')
 bot.send_message(chat_id='@AVMChannel', text="Привет, проверка связи")
+bot.send_document(chat_id='@AVMChannel',document=open('nasa_epic_images/nasa_epic_0.png','rb'))
